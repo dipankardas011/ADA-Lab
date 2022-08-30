5bd60b2babefc10394df4ae6de6dea5e4f270bf6
Dipankar Das <dipankardas0115@gmail.com>
2022-08-30 16:52:15 +0530

    Lab06 Done

--- Lab06/q1.c
+++ Lab06/q1.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char **argv) {
-  int arr[] = {234,236,345,14,3465,4234,23};
-  int n = sizeof(arr)/sizeof(int);
+int COUNT = 0;
+
+void exec(int *arr, int n) {
   int max = arr[0];
   int min = arr[0];
   for (int i=1;i<n;i++) {
-    if (max < arr[i])
+    COUNT++;
+    if (max < arr[i]) {
       max = arr[i];
-    if (min > arr[i])
+    }
+    COUNT++;
+    if (min > arr[i]) {
       min = arr[i];
+    }
   }
-  printf("Smallest: %d\tand largest element: %d\n", min,max);
+}
+
+void executor(int N) {
+  int arr[N];
+  for (int i = 0;i < N;i++)
+    arr[i] = rand();
+  exec(arr, N);
+  printf("Comparison of inp: %d\ttook: %d\n", N, COUNT);
+  COUNT = 0;
+}
+
+int main(int argc, char **argv) {
+  executor(100);
+  executor(1000);
+  executor(10000);
+  executor(100000);
+  executor(1000000);
+
   remove(argv[0]);
   return EXIT_SUCCESS;
 }
--- Lab06/q2.c
+++ Lab06/q2.c
@@ -1,52 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int max(int a, int b) {
-  return a > b ? a : b;
-}
 
-int min(int a, int b) {
-  return a < b ? a : b;
-}
+struct Result {
+  int min, max;
+};
 
-int COUNT_MAX_CMP = 0;
-int COUNT_MIN_CMP = 0;
+int COUNTER = 0;
 
-// FIXME: add in same function to reduce the comparation
-int findSmallest(int* a, int left, int right) {
+struct Result findMinMax(int *a, int left, int right) {
   if (right-left <= 1) {
-    COUNT_MIN_CMP++;
-    return min(a[left], a[right]);
-  } else {
-    int mid = left + (right-left)/2;
-    int x = findSmallest(a, left, mid);
-    int y = findSmallest(a, mid+1, right);
+    COUNTER++;
+    struct Result result;
+    if (a[left] > a[right]) {
+      result.max = a[left];
+      result.min = a[right];
+    } else {
+      result.min = a[left];
+      result.max = a[right];
+    }
+    return result;
+  }else {
+    int mid = left + (right - left)/2;
+    struct Result x = findMinMax(a, left, mid);
+    struct Result y = findMinMax(a, mid+1, right);
 
-    COUNT_MIN_CMP++;
-    return min(x,y);
+    struct Result res;
+    COUNTER++;
+    res.min = x.min < y.min ? x.min : y.min;
+    res.max = x.max > y.max ? x.max : y.max;
+
+    return res;
   }
 }
 
-int findLargest(int* a, int left, int right) {
-  if (right-left <= 1) {
-    COUNT_MAX_CMP++;
-    return max(a[left], a[right]);
-  } else {
-    int mid = left + (right-left)/2;
-    int x = findLargest(a, left, mid);
-    int y = findLargest(a, mid+1, right);
 
-    COUNT_MAX_CMP++;
-    return max(x,y);
-  }
+void executor(int N) {
+  int arr[N];
+  for (int i = 0;i < N;i++)
+    arr[i] = rand();
+  struct Result res = findMinMax(arr,0, N-1);
+  printf("Comparison of inp: %d\ttook: %d\n", N, COUNTER);
+  COUNTER = 0;
 }
 
 int main(int argc, char **argv) {
-  int arr[] = {234,3453,342,64,345,21,34,23};
-  int n = sizeof(arr)/sizeof(arr[0]);
-
-  printf("count-Compare: %d\tSmallest element: %d\n",COUNT_MIN_CMP, findSmallest(arr,0, n-1));
-  printf("count-Compare: %d\tLargest element: %d\n", COUNT_MAX_CMP, findLargest(arr,0, n-1));
+  executor(100);
+  executor(1000);
+  executor(10000);
+  executor(100000);
+  executor(1000000);
 
   remove(argv[0]);
   return EXIT_SUCCESS;
