0b6a47d28079b84ebd2012d237a16f42be68fc5d
Dipankar Das <dipankardas0115@gmail.com>
2022-09-09 23:06:22 +0530

    Added the minimum number of tents required for N student groups
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- Theory/Greedy/main.py
+++ Theory/Greedy/main.py
@@ -1,4 +1,6 @@
-def getMinNoOfPandals(a: list):
+PANDAL_ACCOMODATIOM = 6
+
+def getMinNoOfPandals(a: list) -> int:
     """To find the no of minimum pandals required if all the pandals
     can accomodate max of 6 events
     """
@@ -6,33 +8,90 @@ def getMinNoOfPandals(a: list):
     counter: int
     counter = 0
 
-    # for visitor 
-    visitor = [0] * length
+    # for visitor
+    visited = [False] * length
+    itemsLeft = length
+    j = 0
+    visited[0] = True
+    res = []
+    while itemsLeft > 1:
+        counter += 1
+
+        currCount = 0
+        for i in range (1, length):
+            if visited[i] == False:
+                itemsLeft -= 1
+                if a[j]['end'] <= a[i]['start']:
+                    visited[i] = True
+                    currCount += 1
+                    res.append(a[i]['id'])
+                    j = i
+                    if currCount == PANDAL_ACCOMODATIOM:
+                        break
+    print(res)
+    return counter
 
 
 if __name__ == '__main__':
     """Main Func"""
     arr = [
         {
+            'id': 'a',
+            'start' : 10,
+            'end' : 13
+        },
+        {
+            'id': 'b',
             'start' : 2,
+            'end' : 6
+        },
+        {
+            'id': 'c',
+            'start' : 3,
             'end' : 5
         },
         {
-            'start' : 9,
-            'end' : 10
+            'id': 'd',
+            'start' : 7,
+            'end' : 9
         },
         {
+            'id': 'e',
             'start' : 11,
+            'end' : 14
+        },
+        {
+            'id': 'f',
+            'start' : 21,
+            'end' : 23
+        },
+        {
+            'id': 'g',
+            'start' : 19,
+            'end' : 21
+        },
+        {
+            'id': 'h',
+            'start' : 5,
+            'end' : 8
+        },
+        {
+            'id': 'i',
+            'start' : 10,
             'end' : 15
         },
         {
-            'start' : 16,
-            'end' : 16
+            'id': 'j',
+            'start' : 14,
+            'end' : 18
         },
         {
-            'start' : 20,
-            'end' : 22
+            'id': 'k',
+            'start' : 4,
+            'end' : 9
         }
     ]
-    getMinNoOfPandals(arr)
+    sortedArr = sorted(arr, key=lambda d: d['end'])
+    print(sortedArr)
+    print("Minimum number of pandals: %d" % getMinNoOfPandals(sortedArr))
 
